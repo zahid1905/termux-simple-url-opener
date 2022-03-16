4b6d0bb97f1725c484b1817f68ad27ae2eac7d99
zahid1905 <superpichi.chido@gmail.com>
2022-03-15 20:07:18 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+
+echo -e "Actualizando paquetes por defecto\n"
+pkg upgrade -y
+ 
+echo -e "Solicitando permiso de almacenamiento\n"
+termux-setup-storage
+sleep 5
+ 
+echo -e "Instalando Python\n"
+pkg install python ffmpeg
+
+echo -e "Instalando yt-dlp\n"
+pip install yt-dlp
+ 
+echo -e "Creando el folder bin\n"
+mkdir ~/bin
+ 
+echo -e "Descargando e installando termux-url-opener\n"
+wget --no-check-certificate "https://raw.githubusercontent.com/zahid1905/termux-simple-url-opener/main/termux-url-opener" -O ~/bin/termux-url-opener
+chmod +x ~/bin/termux-url-opener
+
+echo -e "\n"
+echo -e "\n"
+echo -e "Los archivos descargados pueden ser encontrados en la carpeta descargas\n"
+echo -e "\n"
